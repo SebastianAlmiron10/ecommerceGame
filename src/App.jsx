c8487e5c4ad2e1a52163ecf2e30f9a7aa7dd317e
Sebastian Almiron <sebastianr_almiron@hotmail.com>
2024-04-06 18:38:31 -0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [Data, setData] = useState([]);
+
+  useEffect(() => {
+    
+
+
+    return () => {
+      
+    };
+  }, []);
+
+  return (
+    <div className='text-2xl text-white'>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/gameItem/GameItem.jsx
@@ -0,0 +1,20 @@
+import { Button, Card } from "react-bootstrap";
+
+const GameItem = (imageUrl) => {
+
+    <Card style={{ width: "22rem" }} className="m-3">
+            <Card.Img
+                height={400}
+                variant="top"
+                src={imageUrl !== "" ? imageUrl : "https://bit.ly/47NylZk"}
+            />
+            
+            </Card>
+
+
+}
+
+
+
+
+
